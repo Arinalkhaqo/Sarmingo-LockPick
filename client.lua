65210ebb150fd1cf92a9261ec1ea133da060a7a9
Sarmingoo <78432708+Sarmingo@users.noreply.github.com>
2022-11-27 13:38:41 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,11 +1,11 @@
 ESX = exports['es_extended']:getSharedObject()
 
-RegisterCommand('obijauto', function()
-    local kordinateigraca = GetEntityCoords(PlayerPedId())
-    vozilo = GetClosestVehicle(kordinateigraca.x, kordinateigraca.y, kordinateigraca.z, 5.0, 0, 71)
-    ESX.TriggerServerCallback('provjera', function(check)
+RegisterCommand('lockpick', function()
+    local coords = GetEntityCoords(PlayerPedId())
+    vehicle = GetClosestVehicle(coords.x, coords.y, coords.z, 5.0, 0, 71)
+    ESX.TriggerServerCallback('check', function(check)
         if check then
-    if IsAnyVehicleNearPoint(kordinateigraca.x, kordinateigraca.y, kordinateigraca.z, 5.0) then
+    if IsAnyVehicleNearPoint(coords.x, coords.y, coords.z, 5.0) then
         RequestAnimDict('anim@amb@clubhouse@tutorial@bkr_tut_ig3@')
         while not HasAnimDictLoaded('anim@amb@clubhouse@tutorial@bkr_tut_ig3@') do
             Citizen.Wait(0)
@@ -13,19 +13,21 @@ RegisterCommand('obijauto', function()
         TaskPlayAnim(PlayerPedId(), 'anim@amb@clubhouse@tutorial@bkr_tut_ig3@' , 'machinic_loop_mechandplayer' ,8.0, -8.0, -1, 1, 0, false, false, false)
         local success = lib.skillCheck({'easy', 'easy', {areaSize = 60, speedMultiplier = 1}, 'easy'})
         if success then
-            SetVehicleDoorsLockedForAllPlayers(vozilo, false)
+            SetVehicleDoorsLockedForAllPlayers(vehicle, false)
             ClearPedTasksImmediately(PlayerPedId())
+            ESX.ShowNotification('Lockpicked successfully')
         else
             ClearPedTasksImmediately(PlayerPedId())
-            SetVehicleAlarm(vozilo, true)
-            SetVehicleAlarmTimeLeft(vozilo, 60 * 1000)
-            TriggerServerEvent('ukloni')
+            SetVehicleAlarm(vehicle, true)
+            SetVehicleAlarmTimeLeft(vehicle, 60 * 1000)
+            TriggerServerEvent('remove')
+            ESX.ShowNotification('Run, the police have been called')
         end
     else
-    ESX.ShowNotification('Nema vozila u blizini')
+    ESX.ShowNotification('There are no vehicles nearby')
     end
 else
-    ESX.ShowNotification('Nemas lockpick')
+    ESX.ShowNotification("You don't have a lockpick")
     end
 end)
 end)
