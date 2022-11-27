1ccf3eeec1c2959d971641deb9f10a1361f93e59
Sarmingoo <78432708+Sarmingo@users.noreply.github.com>
2022-11-27 13:38:59 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,6 +1,6 @@
 ESX = exports['es_extended']:getSharedObject()
 
-ESX.RegisterServerCallback('provjera', function(source,cb,item)
+ESX.RegisterServerCallback('check', function(source,cb,item)
 	local xPlayer = ESX.GetPlayerFromId(source)
 	if xPlayer.getInventoryItem('lockpick').count >= 1 then
 		cb(true)
@@ -9,7 +9,7 @@ ESX.RegisterServerCallback('provjera', function(source,cb,item)
 	end
 end)
 
-RegisterServerEvent('ukloni', function()
+RegisterServerEvent('remove', function()
 	local xPlayer = ESX.GetPlayerFromId(source)
 	xPlayer.removeInventoryItem('lockpick', 1)
 end)
